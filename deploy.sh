dc57a2929455a3b0e6c99d9316eb8c0661829ef0
DoOneThingAtATime <guakun@outlook.com>
2019-07-08 09:29:54 +0800

    更新部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,21 @@
-yarn build
-git checkout gh-pages
-mv -f dist/* ./
-git add .
-git commit -m "update page"
-git push
-git checkout -
+#!/usr/bin/env sh
+
+# 当发生错误时中止脚本
+set -e
+
+# 构建
+npm run build
+
+# cd 到构建输出的目录下 
+cd dist
+
+git init
+git config user.email 'guakun@outlook.com'
+git config user.name 'DoOneThingAtATime'
+git add -A
+git commit -m 'update page'
+
+# 部署到 https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/DoOneThingAtATime/css-collect.git master:gh-pages
+
+cd -
